0c4d199f8f0753443ef44c276d5693112e82a54f
Richard Palacios G <apalaciosg91@gmail.com>
2022-09-25 05:26:42 -0500

    chore(Root): add react and image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from "astro/config";
-
-import sitemap from "@astrojs/sitemap";
+import image from '@astrojs/image'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [sitemap()],
-});
+  site: 'https://example.com',
+  integrations: [sitemap(), react(), image()]
+})
