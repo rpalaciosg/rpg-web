9e969078c9b4645379d0db421b5dd3b4dbe85241
Richard Palacios G <apalaciosg91@gmail.com>
2022-09-26 00:31:46 -0500

    chore(Root): add tailwind and custom inter-ui font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import image from '@astrojs/image'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap(), react(), image()]
+  integrations: [sitemap(), react(), image(), tailwind()]
 })
